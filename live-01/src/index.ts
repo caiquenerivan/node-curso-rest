d741bdd446c7540eb182b96dce27d998986e7fba
Caique Nerivan <caiquenerivansantos@gmail.com>
2022-03-16 16:27:14 -0300

    finalizando aula 1

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,11 +1,21 @@
-import express, {Request, Response, NextFunction}from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction)=>{
-    res.status(200).send({foo: 'sucesso total'});
-});
+//Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000,()=>{
-    console.log('Aplicação executando na porta 3000'); 
+//Configurações de Rota
+app.use(statusRoute);
+app.use(usersRoute);
+
+
+
+//Inicialização do servidor
+
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000');
 });
